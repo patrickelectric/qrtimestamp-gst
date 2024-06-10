3c6d703ded5f7b7a2fa85d2a276aacf88545c2c4
Patrick José Pereira <patrickelectric@gmail.com>
2024-06-10 16:32:09 -0300

    tester: Remove drag and drop
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/app/imp/wrap.rs
+++ src/app/imp/wrap.rs
@@ -74,8 +74,6 @@ pub struct State {
 
 pub struct WrapApp {
     state: State,
-
-    dropped_files: Vec<egui::DroppedFile>,
 }
 
 impl WrapApp {
@@ -86,8 +84,6 @@ impl WrapApp {
         #[allow(unused_mut)]
         let mut slf = Self {
             state: State::default(),
-
-            dropped_files: Default::default(),
         };
 
         if let Some(storage) = cc.storage {
@@ -149,8 +145,6 @@ impl eframe::App for WrapApp {
         self.show_selected_app(ctx, frame);
 
         self.state.backend_panel.end_of_frame(ctx);
-
-        self.ui_file_drag_and_drop(ctx);
 
         self.run_cmd(ctx, cmd);
     }
@@ -276,81 +270,5 @@ impl WrapApp {
         ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
             egui::warn_if_debug_build(ui);
         });
-    }
-
-    fn ui_file_drag_and_drop(&mut self, ctx: &egui::Context) {
-        use egui::*;
-        use std::fmt::Write as _;
-
-        // Preview hovering files:
-        if !ctx.input(|i| i.raw.hovered_files.is_empty()) {
-            let text = ctx.input(|i| {
-                let mut text = "Dropping files:\n".to_owned();
-                for file in &i.raw.hovered_files {
-                    if let Some(path) = &file.path {
-                        write!(text, "\n{}", path.display()).ok();
-                    } else if !file.mime.is_empty() {
-                        write!(text, "\n{}", file.mime).ok();
-                    } else {
-                        text += "\n???";
-                    }
-                }
-                text
-            });
-
-            let painter =
-                ctx.layer_painter(LayerId::new(Order::Foreground, Id::new("file_drop_target")));
-
-            let screen_rect = ctx.screen_rect();
-            painter.rect_filled(screen_rect, 0.0, Color32::from_black_alpha(192));
-            painter.text(
-                screen_rect.center(),
-                Align2::CENTER_CENTER,
-                text,
-                TextStyle::Heading.resolve(&ctx.style()),
-                Color32::WHITE,
-            );
-        }
-
-        // Collect dropped files:
-        ctx.input(|i| {
-            if !i.raw.dropped_files.is_empty() {
-                self.dropped_files = i.raw.dropped_files.clone();
-            }
-        });
-
-        // Show dropped files (if any):
-        if !self.dropped_files.is_empty() {
-            let mut open = true;
-            egui::Window::new("Dropped files")
-                .open(&mut open)
-                .show(ctx, |ui| {
-                    for file in &self.dropped_files {
-                        let mut info = if let Some(path) = &file.path {
-                            path.display().to_string()
-                        } else if !file.name.is_empty() {
-                            file.name.clone()
-                        } else {
-                            "???".to_owned()
-                        };
-
-                        let mut additional_info = vec![];
-                        if !file.mime.is_empty() {
-                            additional_info.push(format!("type: {}", file.mime));
-                        }
-                        if let Some(bytes) = &file.bytes {
-                            additional_info.push(format!("{} bytes", bytes.len()));
-                        }
-                        if !additional_info.is_empty() {
-                            info += &format!(" ({})", additional_info.join(", "));
-                        }
-
-                        ui.label(info);
-                    }
-                });
-            if !open {
-                self.dropped_files.clear();
-            }
-        }
     }
 }
